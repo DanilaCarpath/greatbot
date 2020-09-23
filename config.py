a641191bd581bf52d08b625c0948aa2f22af14ef
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-09-24 03:17:26 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-settings = {'token': 'ddfdfgf','bot': 'Великий Бот','id': 757919225561219123,'prefix': 'g'
+settings = {'token': 'ddfdfgf','bot': 'Великий Бот','id': 757919225561219123,'prefix': 'М'
 }
