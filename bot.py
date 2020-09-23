d695a5ad02d199140510dba8ed8df6357820b818
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-09-23 07:03:52 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,17 @@ async def c (ctx, _message):
 @bot.event
 async def on_message(message):
 
+    
+
+    #переменные
+    membr = message.author
+    avatar = membr.avatar_url
+    #сообщение с упоминанием автора
+    messageAuthorTemp = '{0.author}'.format(message)
+    messageAuthor = messageAuthorTemp[:-5]
+        
+
+    messageContent = '{0.content}'.format(message)
     #временный сервер для проверки канала
     tempguild = message.author.guild
 
@@ -37,9 +48,14 @@ async def on_message(message):
         
         #если автор - не бот
         else:
+        
+            await message.delete ()
 
-            #сообщение с упоминанием автора
-            newmessage = '{0.author}: {0.content}'.format(message)
+            embed = discord.Embed (color=0x6600ff)
+            embed.set_author(name= messageAuthor, icon_url = avatar)
+            embed.add_field (name = messageContent, value = message.author.guild, inline = True)
+
+            
 
             #цикл на все сервера
             for k in bot.guilds:
@@ -47,16 +63,13 @@ async def on_message(message):
                 #канал, в который отправим
                 channel = discord.utils.get (k.channels, name = "international")
 
-                #проверка, чтобы не отправлялось на сервер автора
-                if k != message.guild:
 
-                    #проверка на наличие канала
-                    try:
-
-                        #отправка сообщения
-                        await channel.send (newmessage)
-                    except:
-                        continue
+                #проверка на наличие канала
+                try:
+                    #отправка сообщения
+                    await channel.send (embed = embed)
+                except:
+                    continue
 
 token = os.environ.get('BOT_TOKEN')
 bot.run (str(token))
