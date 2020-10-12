3d5a56c80d14d654aa0f5055c59afed077949213
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-12 21:43:22 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def on_message(message):
         for i in maxis:
             if i in message.content:
                 await message.channel.send (message.author.mention + " Макси? Макси сосет хуй")
-    louis = ["луи", "Луи", "Лу", "лу", "ЛУИ"]
+    louis = ["Лу", "лу", "ЛУ"]
     
     if message.author.bot:
         print ("bot")
