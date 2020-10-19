007b7d3d52c5a09a236023c582d02465856da837
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-19 22:51:54 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,20 @@ import os
 import random
 from discord.ext.commands import has_permissions, MissingPermissions
 
+from discord.utils import get
+
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.event
 async def on_message(message):
+    fchannel = bot.get_channel(730634007284285545)
+    tchannel = bot.get_channel(765561417842688040)
+    webhook_id = 767706987110072340
+    hooks = await tchannel.webhooks()
+    hook = get(hooks, id=webhook_id)  
+    if message.channel == fchannel:
+        await hook.send(content=message.content, username=message.author.display_name, 
+                        avatar_url=message.author.avatar_url)
           
     messcont = str(message.content)
     mess = messcont.lower () 
@@ -88,10 +98,6 @@ async def on_message(message):
 
     if message.author.id == 767411136035160064:
         await message.delete ()
-
-    if "sendwebhook" in message.content:
-        webhook = discord.Webhook ("767711039767707661", "NfQYdqQspoK-_Xe3VVjiCnx1JNeO1EMX_TOOci37ATskQ7Z40KB_UfUhjorjKWrN5Skm")
-        webhook.send ("odgll")
 
 token = os.environ.get('BOT_TOKEN')
 bot.run (str(token))
