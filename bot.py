9e0743669f07a3ddfe3d82564f88cc7d44c0a99b
DanilaCarpath <71142110+DanilaCarpath@users.noreply.github.com>
2020-10-19 22:54:17 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,13 @@ bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.event
 async def on_message(message):
-    fchannel = bot.get_channel(730634007284285545)
     tchannel = bot.get_channel(765561417842688040)
     webhook_id = 767706987110072340
     hooks = await tchannel.webhooks()
     hook = get(hooks, id=webhook_id)  
-    if message.channel == fchannel:
-        await hook.send(content=message.content, username=message.author.display_name, 
-                        avatar_url=message.author.avatar_url)
+    
+    await hook.send(content=message.content, username=message.author.display_name, 
+                    avatar_url=message.author.avatar_url)
           
     messcont = str(message.content)
     mess = messcont.lower () 
